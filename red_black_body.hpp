655edfd43f4935fe127822c915e548568653bb5e
Saad Akllam <sakllam@e3r7p11.1337.ma>
2022-08-23 10:44:10 +0100

    logic was desined

--- red_black_body.hpp
+++ red_black_body.hpp
@@ -6,7 +6,7 @@
 /*   By: sakllam <sakllam@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/22 18:06:02 by sakllam           #+#    #+#             */
-/*   Updated: 2022/08/22 20:38:58 by sakllam          ###   ########.fr       */
+/*   Updated: 2022/08/23 10:42:43 by sakllam          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,11 @@
 #include <cstdlib>
 #include <memory>
 #include <sys/_types/_size_t.h>
+#define red true
+#define black false
+#define root 1
+#define l 2
+#define r 3
 namespace ft
 {
     template <class T>
@@ -25,8 +30,9 @@ namespace ft
         RedBlackTree *left;
         RedBlackTree *right;
         RedBlackTree *parent;
+        int           position;
         bool          color; // red == true; black == false
-        RedBlackTree() : value(type_name()), left(NULL), right(NULL), parent(NULL), color(true) {}
+        RedBlackTree() : value(type_name()), left(NULL), right(NULL), parent(NULL), color(red) {}
     };
 
     template <class T, class Compare = std::less<T>, class Alloc = std::allocator<RedBlackTree<T> > >
@@ -62,30 +68,63 @@ namespace ft
                 return max(hight(head->color), hight(head->right)) + 1;
             return max(hight(head->color), hight(head->right));
         }
-        void    insert(RedBlackTree<type_name> **head, RedBlackTree<type_name> *nv)
+        void    recoloring(RedBlackTree<type_name> *paint)
+        {
+            do {
+                if (paint->left)
+                    paint->left->color = !(paint->left->color);
+                if (paint->right)
+                    paint->right->color = !(paint->right->color);
+                if (paint->parent->color == root)
+                    break;
+                paint->parent->color = !(paint->parent->color);
+                paint->parent = paint->parent->parent;
+            }
+            while (1);
+        }
+        // void retation
+        void    insert(RedBlackTree<type_name> **head, RedBlackTree<type_name> *nv, int position)
         {
             if (*head == NULL)
             {
                 *head = nv;
+                nv->position = position;
                 if (!size)
-                    nv->color = false;
+                    nv->color = black;
                 return;
             }
             if (cmpr(nv->value, (*head)->value))
-                insert(&((*head)->left), nv);
+                insert(&((*head)->left), nv, r);
             else
-                insert(&((*head)->right), nv);
-            (*head)->->parent = 
-            // if (abs(hight((*head)->right) - hight((*head)->right)) > 0)  // check for balencing and colors
-            // {
-                
-            // }
+                insert(&((*head)->right), nv, l);
+            if ((*head)->left)
+                (*head)->left->parent = *head;
+            if ((*head)->right)
+                (*head)->right->parent = *head;
+            if ((*head)->color == red)
+            {
+               if (position == r)
+               {
+                   if ((*head)->parent->left && (*head)->parent->left->color == red)
+                        recoloring((*head)->parent);
+                   else
+                        rotation((*head)->parent);
+               }
+               else
+               {
+                    if ((*head)->parent->left && (*head)->parent->left->color == red)
+                        recoloring((*head)->parent);
+                    else
+                        rotation((*head)->parent);
+               }
+            }
         }
         public:
             RBT() : head(NULL), size(0) {}
             void insert(type_name value)
             {
-                insert(&head, newnode(value));
+                insert(&head, newnode(value), root);
+                size++;
             }
     };
 }
